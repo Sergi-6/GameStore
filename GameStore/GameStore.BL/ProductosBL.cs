e22a20dfdd36fe62ca0f0ce4ffab4af14f6569e2
Sergi Flores <flores.sergy2262@gmail.com>
2021-10-16 10:16:19 -0600

    Agregando imagenes y validaciones a la lista de productos

--- GameStore/GameStore.BL/ProductosBL.cs
+++ GameStore/GameStore.BL/ProductosBL.cs
@@ -36,8 +36,11 @@ namespace GameStore.BL
             else
             {
                 var productoExistente = _contexto.Productos.Find(producto.Id);
+
                 productoExistente.Descripcion = producto.Descripcion;
+                productoExistente.CategoriaId = producto.CategoriaId;
                 productoExistente.Precio = producto.Precio;
+                productoExistente.UrlImagen = producto.UrlImagen;
             }
            
             _contexto.SaveChanges();
--- GameStore/GameStore.WebAdmin/Controllers/ProductosController.cs
+++ GameStore/GameStore.WebAdmin/Controllers/ProductosController.cs
@@ -39,11 +39,34 @@ namespace GameStore.WebAdmin.Controllers
         }
 
         [HttpPost]
-        public ActionResult Crear(Producto producto)
+        public ActionResult Crear(Producto producto, HttpPostedFileBase imagen)
         {
-            _productosBL.GuardarProducto(producto);
+            if (ModelState.IsValid)
+            {
+                if (producto.CategoriaId == 0)
+                {
+                    ModelState.AddModelError("CategoriaId", "Seleccione una categoria");
+                    return View(producto);
+                }
 
-            return RedirectToAction("Index");
+                if (imagen != null)
+                {
+                    producto.UrlImagen = GuardarImagen(imagen);
+                }
+
+                _productosBL.GuardarProducto(producto);
+
+                return RedirectToAction("Index");
+            }
+
+            var categorias = _categoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId =
+                new SelectList(categorias, "Id", "Descripcion");
+
+
+            return View(producto);
+            
         }
 
         public ActionResult Editar(int id)
@@ -60,8 +83,26 @@ namespace GameStore.WebAdmin.Controllers
         [HttpPost]
         public ActionResult Editar (Producto producto)
         {
-            _productosBL.GuardarProducto(producto);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                if (producto.CategoriaId == 0)
+                {
+                    ModelState.AddModelError("CategoriaId", "Seleccione una categoria");
+                    return View(producto);
+                }
+
+                _productosBL.GuardarProducto(producto);
+
+                return RedirectToAction("Index");
+            }
+
+            var categorias = _categoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId =
+                new SelectList(categorias, "Id", "Descripcion");
+
+
+            return View(producto);
         }
 
         public ActionResult Detalle(int id)
@@ -83,6 +124,14 @@ namespace GameStore.WebAdmin.Controllers
             _productosBL.EliminarProducto(producto.Id);
 
             return RedirectToAction("Index");
+        }
+
+        private string GuardarImagen(HttpPostedFileBase imagen)
+        {
+            string path = Server.MapPath("~/Imagenes/" + imagen.FileName);
+            imagen.SaveAs(path);
+
+            return "/Imagenes/" + imagen.FileName;
         }
     }
 }
