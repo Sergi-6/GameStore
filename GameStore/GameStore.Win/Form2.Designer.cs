d585f788554e5f08a1d5bc853e008f4172c6c95c
Sergi Flores <flores.sergy2262@gmail.com>
2021-11-14 18:48:59 -0600

    Agregando plantilla y modificando sitio web

--- GameStore/GameStore.Win/Form2.Designer.cs
+++ GameStore/GameStore.Win/Form2.Designer.cs
@@ -89,7 +89,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 319);
+            this.ClientSize = new System.Drawing.Size(382, 310);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listadeVentasPorProductoDataGridView);
             this.Name = "Form2";
